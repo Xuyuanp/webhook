377a7ad425e58a9f34c12915a7f9932d31305706
Xuyuan Pang <xuyuanp@gmail.com>
2015-04-02 21:28:01 +0800

    webhook as a service

---
+++ event.go
@@ -0,0 +1,110 @@
+package webhook
+
+// Repository struct
+type Repository struct {
+	Name        string `json:"name"`
+	URL         string `json:"url"`
+	Description string `json:"description"`
+	Homepage    string `json:"homepage"`
+}
+
+// Commit struct
+type Commit struct {
+	ID        string `json:"id"`
+	Message   string `json:"message"`
+	Timestamp string `json:"timestamp"`
+	URL       string `json:"url"`
+	Author    Author `json:"author"`
+}
+
+// Author struct
+type Author struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+// PushEvent struct
+type PushEvent struct {
+	Before            string     `json:"before"`
+	After             string     `json:"after"`
+	Ref               string     `json:"ref"`
+	UserID            int        `json:"user_id"`
+	UserName          string     `json:"user_name"`
+	ProjectID         int        `json:"project_id"`
+	Repository        Repository `json:"repository"`
+	Commits           []Commit   `json:"commits"`
+	TotalCommitsCount int        `json:"total_commits_count"`
+}
+
+// User struct
+type User struct {
+	Name      string `json:"name"`
+	UserName  string `json:"username"`
+	AvatarURL string `json:"avatar_url"`
+}
+
+// IssuesObjectAttributes struct
+type IssuesObjectAttributes struct {
+	ID          int    `json:"id"`
+	Title       string `json:"title"`
+	AssigneeID  int    `json:"assigneee_id"`
+	AuthorID    int    `json:"author_id"`
+	ProjectID   int    `json:"project_id"`
+	CreatedAt   string `json:"created_at"`
+	UpdatedAt   string `json:"updated_at"`
+	Position    string `json:"position"`
+	BranchName  string `json:"branch_name"`
+	Description string `json:"description"`
+	MilestoneID int    `json:"milestone_id"`
+	State       string `json:"state"`
+	IID         int    `json:"iid"`
+	URL         string `json:url`
+	Action      string `json:action`
+}
+
+// IssuesEvent struct
+type IssuesEvent struct {
+	ObjectKind       string                 `json:"object_kind"`
+	User             User                   `json:"user"`
+	ObjectAttributes IssuesObjectAttributes `json:"object_attributes"`
+}
+
+// Peer struct
+type Peer struct {
+	Name            string `json:"name"`
+	SSHURL          string `json:"ssh_url"`
+	HTTPURL         string `json:"http_url"`
+	VisibilityLevel int    `json:"visibility_level"`
+	Namespace       string `json:"namespace"`
+}
+
+// MergeRequestObjectAttributes struct
+type MergeRequestObjectAttributes struct {
+	ID              int    `json:"id"`
+	TargetBranch    string `json:"target_branch"`
+	SourceBranch    string `json:"source_branch"`
+	SourceProjectID int    `json:"source_project_id"`
+	AuthorID        int    `json:"author_id"`
+	AssigneeID      int    `json:"assigneee_id"`
+	Title           string `json:"title"`
+	CreatedAt       string `json:"created_at"`
+	UpdatedAt       string `json:"updated_at"`
+	StCommits       string `json:"st_commits"`
+	StDiffs         string `json:"st_diffs"`
+	MilestoneID     int    `json:"milestone_id"`
+	State           string `json:"state"`
+	MergeStatus     string `json:"merge_status"`
+	TargetProjectID int    `json:"target_project_id"`
+	IID             int    `json:"iid"`
+	Description     string `json:"description"`
+	Source          Peer   `json:"source"`
+	Target          Peer   `json:"target"`
+	LastCommit      Commit `json:"last_commit"`
+}
+
+// MergeRequestEvent struct
+type MergeRequestEvent struct {
+	ObjectKind       string                       `json:"object_kind"`
+	User             User                         `json:"user"`
+	ObjectAttributes MergeRequestObjectAttributes `json:"object_attributes"`
+}
--- webhook.go
+++ webhook.go
@@ -1,186 +1,109 @@
-package main
+package webhook
 
 import (
 	"encoding/json"
-	"flag"
 	"io"
 	"io/ioutil"
-	"log"
 	"net/http"
-	"os"
-	"os/exec"
 )
 
-var updateScript = flag.String("update-script", "", "update shell path")
+// PushEventHandler function
+type PushEventHandler func(event *PushEvent)
 
-// Repository struct
-type Repository struct {
-	Name        string `json:"name"`
-	URL         string `json:"url"`
-	Description string `json:"description"`
-	Homepage    string `json:"homepage"`
+// PushTagEventHandler function
+type PushTagEventHandler func(evetn *PushEvent)
+
+// IssuesEventHandler function
+type IssuesEventHandler func(event *IssuesEvent)
+
+// MergeRequestEventHandler function
+type MergeRequestEventHandler func(event *MergeRequestEvent)
+
+// WebHook struct
+type WebHook struct {
+	PushEventHandler         PushEventHandler
+	PushTagEventHandler      PushTagEventHandler
+	IssuesEventHandler       IssuesEventHandler
+	MergeRequestEventHandler MergeRequestEventHandler
+
+	PushEventPath         string
+	PushTagEventPath      string
+	IssuesEventPath       string
+	MergeRequestEventPath string
+
+	ListenAddr string
 }
 
-// Commit struct
-type Commit struct {
-	ID        string `json:"id"`
-	Message   string `json:"message"`
-	Timestamp string `json:"timestamp"`
-	URL       string `json:"url"`
-	Author    Author `json:"author"`
+// Run startup webhook service
+func (wh *WebHook) Run() error {
+	return http.ListenAndServe(wh.ListenAddr, wh)
 }
 
-// Author struct
-type Author struct {
-	Name  string `json:"name"`
-	Email string `json:"email"`
-}
-
-// PushEvent struct
-type PushEvent struct {
-	Before            string     `json:"before"`
-	After             string     `json:"after"`
-	Ref               string     `json:"ref"`
-	UserID            int        `json:"user_id"`
-	UserName          string     `json:"user_name"`
-	ProjectID         int        `json:"project_id"`
-	Repository        Repository `json:"repository"`
-	Commits           []Commit   `json:"commits"`
-	TotalCommitsCount int        `json:"total_commits_count"`
-}
-
-// User struct
-type User struct {
-	Name      string `json:"name"`
-	UserName  string `json:"username"`
-	AvatarURL string `json:"avatar_url"`
-}
-
-// IssuesObjectAttributes struct
-type IssuesObjectAttributes struct {
-	ID          int    `json:"id"`
-	Title       string `json:"title"`
-	AssigneeID  int    `json:"assigneee_id"`
-	AuthorID    int    `json:"author_id"`
-	ProjectID   int    `json:"project_id"`
-	CreatedAt   string `json:"created_at"`
-	UpdatedAt   string `json:"updated_at"`
-	Position    string `json:"position"`
-	BranchName  string `json:"branch_name"`
-	Description string `json:"description"`
-	MilestoneID int    `json:"milestone_id"`
-	State       string `json:"state"`
-	IID         int    `json:"iid"`
-	URL         string `json:url`
-	Action      string `json:action`
-}
-
-// IssuesEvent struct
-type IssuesEvent struct {
-	ObjectKind       string                 `json:"object_kind"`
-	User             User                   `json:"user"`
-	ObjectAttributes IssuesObjectAttributes `json:"object_attributes"`
-}
-
-// Peer struct
-type Peer struct {
-	Name            string `json:"name"`
-	SSHURL          string `json:"ssh_url"`
-	HTTPURL         string `json:"http_url"`
-	VisibilityLevel int    `json:"visibility_level"`
-	Namespace       string `json:"namespace"`
-}
-
-type MergeRequestObjectAttributes struct {
-	ID              int    `json:"id"`
-	TargetBranch    string `json:"target_branch"`
-	SourceBranch    string `json:"source_branch"`
-	SourceProjectID int    `json:"source_project_id"`
-	AuthorID        int    `json:"author_id"`
-	AssigneeID      int    `json:"assigneee_id"`
-	Title           string `json:"title"`
-	CreatedAt       string `json:"created_at"`
-	UpdatedAt       string `json:"updated_at"`
-	StCommits       string `json:"st_commits"`
-	StDiffs         string `json:"st_diffs"`
-	MilestoneID     int    `json:"milestone_id"`
-	State           string `json:"state"`
-	MergeStatus     string `json:"merge_status"`
-	TargetProjectID int    `json:"target_project_id"`
-	IID             int    `json:"iid"`
-	Description     string `json:"description"`
-	Source          Peer   `json:"source"`
-	Target          Peer   `json:"target"`
-	LastCommit      Commit `json:"last_commit"`
-}
-
-func init() {
-	flag.Parse()
-}
-
-func main() {
-	if *updateScript == "" {
-		log.Fatal("update shell required")
+// New return new WebHook
+func New(addr string) *WebHook {
+	return &WebHook{
+		PushEventPath:         "/push",
+		PushTagEventPath:      "/push/tag",
+		IssuesEventPath:       "/issues",
+		MergeRequestEventPath: "/mergerequest",
+		ListenAddr:            addr,
 	}
-
-	_, err := os.Stat(*updateScript)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("/push", pushEventHandler)
-	mux.HandleFunc("/push/tag", pushTagEventHandler)
-
-	log.Fatal(http.ListenAndServe(":12138", mux))
 }
 
-func pushEventHandler(w http.ResponseWriter, req *http.Request) {
-	event, err := parseEvent(req.Body)
-	if err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusBadRequest)
-		return
+func (wh *WebHook) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	path := req.URL.Path
+	switch path {
+	case wh.PushEventPath:
+		if wh.PushEventHandler != nil {
+			event := &PushEvent{}
+			if err := parseEvent(req.Body, event); err != nil {
+				http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+				return
+			}
+			wh.PushEventHandler(event)
+			w.WriteHeader(http.StatusOK)
+		}
+	case wh.PushTagEventPath:
+		if wh.PushTagEventHandler != nil {
+			event := &PushEvent{}
+			if err := parseEvent(req.Body, event); err != nil {
+				http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+				return
+			}
+			wh.PushTagEventHandler(event)
+			w.WriteHeader(http.StatusOK)
+		}
+	case wh.IssuesEventPath:
+		if wh.IssuesEventHandler != nil {
+			event := &IssuesEvent{}
+			if err := parseEvent(req.Body, event); err != nil {
+				http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+				return
+			}
+			wh.IssuesEventHandler(event)
+			w.WriteHeader(http.StatusOK)
+		}
+	case wh.MergeRequestEventPath:
+		if wh.MergeRequestEventHandler != nil {
+			event := &MergeRequestEvent{}
+			if err := parseEvent(req.Body, event); err != nil {
+				http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+				return
+			}
+			wh.MergeRequestEventHandler(event)
+			w.WriteHeader(http.StatusOK)
+		}
+	default:
+		http.NotFound(w, req)
 	}
-
-	if event.Ref != "refs/heads/develop" || event.UserName != "pangxuyuan" || event.UserName != "Xuyuan Pang" {
-		w.WriteHeader(http.StatusNotModified)
-		return
-	}
-
-	cmd := exec.Command("sh", *updateScript)
-	out, err := cmd.Output()
-	if err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	log.Println(string(out))
-	w.WriteHeader(http.StatusOK)
 }
 
-func pushTagEventHandler(w http.ResponseWriter, req *http.Request) {
-	event, err := parseEvent(req.Body)
-	if err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	log.Printf("%v\n", event)
-}
-
-func parseEvent(r io.Reader) (*PushEvent, error) {
+func parseEvent(r io.Reader, event interface{}) error {
 	data, err := ioutil.ReadAll(r)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	event := &PushEvent{}
 	err = json.Unmarshal(data, event)
-	if err != nil {
-		return nil, err
-	}
-	return event, nil
+	return err
 }
